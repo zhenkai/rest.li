56d6f01c016a9ee44131fa47bb491e98c811428d
Zhenkai Zhu <zzhu@linkedin.com>
2015-02-04 21:20:15 -0800

    Change EntityStream API to specify chunk size
    update to use the new Writer API
    
    add shutdownhook for executor

--- r2/src/main/java/com/linkedin/r2/message/streaming/Observer.java
+++ r2/src/main/java/com/linkedin/r2/message/streaming/Observer.java
@@ -3,7 +3,8 @@ package com.linkedin.r2.message.streaming;
 import com.linkedin.data.ByteString;
 
 /**
- * Observer observes the data flow of an EntityStream {@link com.linkedin.r2.message.streaming.EntityStream}
+ * Observer passively observes the data flow of an EntityStream {@link com.linkedin.r2.message.streaming.EntityStream}
+ * i.e. observer cannot drive the flow of data in the EntityStream
  *
  * @author Zhenkai Zhu
  */
@@ -13,7 +14,7 @@ public interface Observer
    * This is called when a new chunk of data is written to the stream by the writer.
    * @param data data written by the writer
    */
-  void onReadPossible(final ByteString data);
+  void onDataAvailable(final ByteString data);
 
   /**
    * This is called when the writer finished writing.
--- r2/src/main/java/com/linkedin/r2/message/streaming/Writer.java
+++ r2/src/main/java/com/linkedin/r2/message/streaming/Writer.java
@@ -11,13 +11,20 @@ public interface Writer
    * This is called when a Reader is set for the EntityStream.
    *
    * @param wh the handle to write data to the EntityStream.
+   * @param chunkSize the desired chunk size for data
    */
-  void onInit(final WriteHandle wh);
+  void onInit(final WriteHandle wh, final int chunkSize);
 
   /**
-   * This is called when the Reader requested the writer to produce more data.
+   * Invoked when it it possible to write data.
    *
-   * @param bytesNum the additional bytes allowed to write
+   * This method will be invoked the first time as soon as data can be written to the WriteHandle.
+   * Subsequent invocations will only occur if a call to WriteHandle.isWritable() has returned false
+   * and it has since become possible to write data.
+   *
+   * This invocation pattern is identical to WriteListener.onWritePossible in Servlet API 3.1 and
+   * maybe Netty 4 ChannelInboundHandler.channelWritabilityChanged; the latter didn't say exactly as above,
+   * but the use pattern is the same (used together with Channel.isWritable).
    */
-  void onWritePossible(final int bytesNum);
+  void onWritePossible();
 }
