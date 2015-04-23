9c56250cbd6612a346e00ddb3a85a94e6abaffa7
Zhenkai Zhu <zzhu@linkedin.com>
2015-04-22 17:12:19 -0700

    rebase from master and fix query tunnel filters

--- r2/src/main/java/com/linkedin/r2/filter/transport/ClientQueryTunnelFilter.java
+++ r2/src/main/java/com/linkedin/r2/filter/transport/ClientQueryTunnelFilter.java
@@ -1,18 +1,19 @@
 package com.linkedin.r2.filter.transport;
 
+import com.linkedin.common.callback.Callback;
 import com.linkedin.r2.filter.NextFilter;
-import com.linkedin.r2.filter.message.rest.RestRequestFilter;
+import com.linkedin.r2.filter.message.rest.StreamRequestFilter;
 import com.linkedin.r2.message.RequestContext;
 import com.linkedin.r2.message.rest.QueryTunnelUtil;
-import com.linkedin.r2.message.rest.RestRequest;
-import com.linkedin.r2.message.rest.RestResponse;
+import com.linkedin.r2.message.rest.StreamRequest;
+import com.linkedin.r2.message.rest.StreamResponse;
 
 import java.util.Map;
 
 /**
  * @author Zhenkai Zhu
  */
-public class ClientQueryTunnelFilter implements RestRequestFilter
+public class ClientQueryTunnelFilter implements StreamRequestFilter
 {
   private final int _queryPostThreshold;
 
@@ -22,21 +23,26 @@ public class ClientQueryTunnelFilter implements RestRequestFilter
   }
 
   @Override
-  public void onRestRequest(RestRequest req,
-                     RequestContext requestContext,
-                     Map<String, String> wireAttrs,
-                     NextFilter<RestRequest, RestResponse> nextFilter)
+  public void onRequest(final StreamRequest req,
+                     final RequestContext requestContext,
+                     final Map<String, String> wireAttrs,
+                     final NextFilter<StreamRequest, StreamResponse> nextFilter)
   {
-    final RestRequest newReq;
-    try
+    Callback<StreamRequest> callback = new Callback<StreamRequest>()
     {
-      newReq = QueryTunnelUtil.encode(req, requestContext, _queryPostThreshold);
-    }
-    catch (Exception e)
-    {
-      nextFilter.onError(e, requestContext, wireAttrs);
-      return;
-    }
-    nextFilter.onRequest(newReq, requestContext, wireAttrs);
+      @Override
+      public void onError(Throwable e)
+      {
+        nextFilter.onError(e, requestContext, wireAttrs);
+      }
+
+      @Override
+      public void onSuccess(StreamRequest newReq)
+      {
+        nextFilter.onRequest(newReq, requestContext, wireAttrs);
+      }
+    };
+
+    QueryTunnelUtil.encode(req, requestContext, _queryPostThreshold, callback);
   }
 }
