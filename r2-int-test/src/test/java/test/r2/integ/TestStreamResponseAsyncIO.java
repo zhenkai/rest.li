ba3167447dfa2b2b4e02f4022a8671de58f5dfbf
Zhenkai Zhu <zzhu@linkedin.com>
2015-05-01 14:14:39 -0700

    simplify AsyncIOResponseHandler to get rid of locks It seems the bug is gone after getting rid of the locks

--- r2-int-test/src/test/java/test/r2/integ/TestStreamResponseAsyncIO.java
+++ r2-int-test/src/test/java/test/r2/integ/TestStreamResponseAsyncIO.java
@@ -7,10 +7,9 @@ import org.testng.annotations.Test;
 /**
  * @author Zhenkai Zhu
  */
-@Test(enabled = false)   // TODO [zz]: enable if we want to support servlet 3.1
-public class TestStreamResponseAsyncIO // extends TestStreamResponse  // disable class didn't work
+@Test
+public class TestStreamResponseAsyncIO  extends TestStreamResponse  // disable class didn't work
 {
-  // @Override
   protected HttpServerFactory getServerFactory()
   {
     return new HttpServerFactory(HttpJettyServer.ServletType.ASYNC_IO);
