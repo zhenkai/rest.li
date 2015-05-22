dc16f500b2813a5de71d0f140078b34ac487b590
Ang Xu <axu@linkedin.com>
2015-05-22 13:29:53 -0700

    Small refactory on streaming compressor

--- r2/src/main/java/com/linkedin/r2/filter/compression/streaming/DeflateCompressor.java
+++ r2/src/main/java/com/linkedin/r2/filter/compression/streaming/DeflateCompressor.java
@@ -60,20 +60,12 @@ public class DeflateCompressor extends AbstractCompressor
   @Override
   protected StreamingDeflater createDeflater()
   {
-    return new StreamingDeflater()
+    return new StreamingDeflater(_threshold)
     {
       @Override
       protected OutputStream createOutputStream(OutputStream out) throws IOException
       {
-        return new DelayedCompressionOutputStream(out, _threshold)
-        {
-          @Override
-          OutputStream compressionOutputStream(OutputStream outputStream)
-              throws IOException
-          {
-            return new DeflaterOutputStream(outputStream);
-          }
-        };
+        return new DeflaterOutputStream(out);
       }
     };
   }
