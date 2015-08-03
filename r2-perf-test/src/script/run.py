4b70f487b8c21f912a7a431a7acf33d9b94c80bb
Zhenkai Zhu <zzhu@linkedin.com>
2015-08-02 22:48:08 -0700

    update python

--- r2-perf-test/src/script/run.py
+++ r2-perf-test/src/script/run.py
@@ -3,13 +3,38 @@ from __future__ import print_function
 import argparse
 from collections import namedtuple
 import json
+import logging
 import os
-from subprocess import Popen, call
-import subprocess
+import re
+from subprocess import Popen, PIPE, call
 import shlex
+from signal import SIGKILL
+import socket
+import sys
+from time import strftime, sleep
+
+logger = logging.getLogger('r2-perf-test')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+error_or_warn = re.compile("WARN|ERROR")
+done = re.compile("DONE")
+empty = re.compile("^\s*$")
+server_started = re.compile("=== Starting Http server ===")
 
 Test = namedtuple("Test", ["name", "client_properties", "server_properties"])
 TestGroup = namedtuple("TestGroup", ["name", "tests"])
+
+def poke(port):
+	s = socket.socket()
+	try:
+		s.connect(('localhost', port))
+		return True
+	except socket.error, e:
+		return False
 
 def read_runbook(runbooks):
 	test_groups = []
@@ -30,18 +55,54 @@ def read_runbook(runbooks):
 
 	return test_groups
 
-def run(directory, test_group):
-	print(directory)
-	print(test_group)
+def run(directory, test_group, gradle, cwd):
+	logger.info("processing test group: {0}".format(test_group.name))
 	file_path = os.path.join(directory, test_group.name)
 	with open(file_path, "w") as out:
-		pass
+		def write_to_file(msg):
+			print(msg, file=out)
+
+		for test in test_group.tests:
+			logger.info("processing test: {0}".format(test.name))
+			write_to_file(test.name)
+			write_to_file("client properties: {0}".format(test.client_properties))
+			write_to_file("server properties: {0}".format(test.server_properties))
+			logger.info("starting server...")
+			server_process = run_gradle(gradle, 'runHttpServer', test.server_properties, cwd)
+
+			i = 0
+			while not poke(8082):
+				i = i + 1
+				assert i < 120, "Server didn't start in 120 seconds"
+				sleep(1)
+
+			logger.info("starting client...")
+			client_process = run_gradle(gradle, 'runHttpRestClient', test.client_properties, cwd)
+			test_done = False
+			for raw_line in client_process.stdout:
+				line = raw_line.rstrip(os.linesep)
+				if error_or_warn.search(line):
+					logger.warn("client error or warn: " + line)
+				elif done.search(line):
+					test_done = True
+
+				if test_done and not empty.match(line):
+					write_to_file(line)
+
+			client_process.wait()
+			logger.info("stopped client...")
+			server_process.kill()
+			server_process.wait()
+			logger.info("stopped server...")
+			write_to_file("============================")
+
+		logger.info("finished processing of test group: {0}".format(test_group.name))
+
 
 def run_gradle(gradle, gradle_cmd, properties, cwd):
 	raw_cmd = "{0} {1} {2}".format(gradle, gradle_cmd, properties)
 	args = shlex.split(raw_cmd)
-	run_server = Popen(args, cwd=cwd)
-	return run_server
+	return Popen(args, cwd=cwd, stdout=PIPE, stderr=PIPE)
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser("run tests according to runbook")
@@ -51,15 +112,23 @@ if __name__ == '__main__':
 	parser.add_argument('--cwd', type=str, default='.', help='child process work directory')
 
 	args = parser.parse_args()
-	directory = args.dir
+	directory = args.out
 	if not os.path.exists(directory):
 		os.makedirs(directory)
 
+	logging_handler = logging.FileHandler(os.path.join(directory, 'run-{0}.log'.format(strftime("%Y-%m-%d_%H_%M_%S"))))
+	logging_handler.setFormatter(formatter)
+	logger.addHandler(logging_handler)
+	logger.setLevel(logging.INFO)
+
 	test_groups = read_runbook(args.runbooks)
-	for test_group in test_groups:
-		run(directory, test_group)
-	# p = run_gradle(args.gradle, 'runHttpServer', '', args.cwd)
-	# p.wait()
+	os.setpgrp() # create new process group, become its leader
+	try:
+		for test_group in test_groups:
+			run(directory, test_group, args.gradle, args.cwd)
+	finally:
+		os.killpg(0, SIGKILL) # kill all processes in this group
+
 
 
 
