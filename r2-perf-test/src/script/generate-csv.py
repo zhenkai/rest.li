e236c018a500f1eed95c17af1b304447c82e9260
Zhenkai Zhu <zzhu@linkedin.com>
2015-08-05 11:12:11 -0700

    fix bug in generate-csv

--- r2-perf-test/src/script/generate-csv.py
+++ r2-perf-test/src/script/generate-csv.py
@@ -19,7 +19,7 @@ def parse(test_output_dir):
 				line = raw_line.strip()
 				if line.find('Mean') > -1:
 					mean = get_float(line)
-				elif line.find('90') > -1:
+				elif line.find('90%') > -1:
 					ninty = get_float(line)
 				elif line.find('Reqs') > -1:
 					qps = get_float(line)
@@ -39,6 +39,8 @@ def get_float(line):
 	return float(str_value.strip())
 
 def generate(baseline_group_name, baseline, results, out_dir):
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
 	for field in ['mean', 'qps', 'ninty']:
 		file_name = os.path.join(out_dir, field)
 		with open(file_name, 'w') as out:
@@ -52,17 +54,22 @@ def generate(baseline_group_name, baseline, results, out_dir):
 
 			for base_name, base_result in baseline.iteritems():
 				line = []
+				line.append(base_name)
 				base_value = getattr(base_result, field)
-				line.append('{0:.3f}'.format(base_value))
+				if field == 'qps':
+					line.append(str(int(base_value)))
+				else:
+					line.append('{0:.3f}'.format(base_value))
 				for (name, test_result_dict) in results:
 					test_result = test_result_dict[base_name]
 					test_value = getattr(test_result, field)
-					line.append('{0:.3f}'.format(test_value))
 					delta = (test_value - base_value) / base_value * 100
-					if (delta > 0):
-						line.append('+{0}%'.format('{0:.1f}'.format(delta)))
+					if delta > 0:
+						format_str = '{0:.3f} (+{1:.1f}%)'
 					else:
-						line.append('{0}%'.format('{0:.1f}'.format(delta)))	
+						format_str = '{0:.3f} ({1:.1f}%)'
+					line.append(format_str.format(test_value, delta))
+
 				print(','.join(line), file=out)
 
 if __name__ == '__main__':
