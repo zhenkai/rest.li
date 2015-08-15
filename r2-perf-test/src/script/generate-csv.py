ed4dda8a612da7df981f3096382325cdd6c0d10a
Zhenkai Zhu <zzhu@linkedin.com>
2015-08-14 23:10:39 -0700

    update script

---
+++ r2-perf-test/src/script/generate-csv.py
@@ -0,0 +1,93 @@
+from __future__ import print_function
+
+import argparse
+from collections import namedtuple
+import glob
+import os
+import re
+
+Result = namedtuple('Result', ['group', 'name', 'median', 'qps', 'ninty'])
+
+def parse(test_output_dir):
+	group_name = re.sub('-result', '', os.path.basename(os.path.normpath(test_output_dir)))
+	results = {}
+	for output_file in glob.glob(os.path.join(test_output_dir, '*.output')):
+		output_file_name = os.path.splitext(os.path.basename(output_file))[0]
+		test_name = re.sub(r"-\d-result", '', output_file_name)
+		with open(output_file, 'r') as src:
+			for raw_line in src:
+				line = raw_line.strip()
+				if line.find('50%') > -1:
+					median = get_float(line)
+				elif line.find('90%') > -1:
+					ninty = get_float(line)
+				elif line.find('Reqs') > -1:
+					qps = get_float(line)
+
+			result = Result(group_name, test_name, median, qps, ninty)
+
+			if results.get(result.name):
+				prev_result = results.get(result.name)
+				results[result.name] = Result(group_name, test_name, (median + prev_result.median)/2, (qps + prev_result.qps)/2, (ninty + prev_result.ninty)/2 )
+			else:
+				results[result.name] = result
+
+	return group_name, results
+
+def get_float(line):
+	str_value = line.split(':')[1]
+	return float(str_value.strip())
+
+def generate(baseline_group_name, baseline, results, out_dir):
+	if not os.path.exists(out_dir):
+		os.makedirs(out_dir)
+	for field in ['median', 'qps', 'ninty']:
+		file_name = os.path.join(out_dir, field)
+		with open(file_name, 'w') as out:
+			headers = []
+			headers.append('Test Name')
+			headers.append(baseline_group_name)
+			for (name, result) in results:
+				headers.append(name)
+
+			print(','.join(headers), file=out)
+
+			for base_name, base_result in baseline.iteritems():
+				line = []
+				line.append(base_name)
+				base_value = getattr(base_result, field)
+				if field == 'qps':
+					line.append(str(int(base_value)))
+				else:
+					line.append('{0:.3f}'.format(base_value))
+				for (name, test_result_dict) in results:
+					test_result = test_result_dict[base_name]
+					test_value = getattr(test_result, field)
+					delta = (test_value - base_value) / base_value * 100
+					if delta > 0:
+						format_str = '{0:.3f} (+{1:.1f}%)'
+					else:
+						format_str = '{0:.3f} ({1:.1f}%)'
+					line.append(format_str.format(test_value, delta))
+
+				print(','.join(line), file=out)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser("generate csv file for comparison")
+	parser.add_argument('-b', '--baseline', type=str, required=True, help='baseline output dir')
+	parser.add_argument('tests', type=str, nargs='+', help='test output dirs')
+	parser.add_argument('--out', type=str, default='./out', help='output dir')
+
+	args = parser.parse_args()
+
+	baseline_group_name, baseline_result = parse(args.baseline)
+        print(baseline_result)
+
+	results = []
+	for test in args.tests:
+		test_group_name, test_results = parse(test)
+                print(test_results)
+		results.append((test_group_name,test_results))
+
+	generate(baseline_group_name, baseline_result, results, args.out)
+
--- r2-perf-test/src/script/run.py
+++ r2-perf-test/src/script/run.py
@@ -2,12 +2,14 @@ from __future__ import print_function
 
 import argparse
 from collections import namedtuple
+import glob
 import json
 import logging
 import os
 import re
-from subprocess import Popen, PIPE, check_call
+from subprocess import Popen, PIPE, check_call, call
 import shlex
+import shutil
 from signal import SIGKILL
 import socket
 import sys
@@ -20,13 +22,14 @@ stream_handler.setLevel(logging.DEBUG)
 stream_handler.setFormatter(formatter)
 logger.addHandler(stream_handler)
 
-error_or_warn = re.compile("WARN|ERROR")
-done = re.compile("DONE")
-empty = re.compile("^\s*$")
-server_started = re.compile("=== Starting Http server ===")
+# hack due to gradle lock issue
+copy_dir = '../../../pegasus_trunk-copy/pegasus/r2-perf-test'
 
 Test = namedtuple("Test", ["name", "client_properties", "server_properties"])
-TestGroup = namedtuple("TestGroup", ["name", "branch", "tests"])
+TestGroup = namedtuple("TestGroup", ["name", "client_branch", "server_branch", "tests"])
+
+def get_current_branch():
+	return Popen(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout=PIPE).communicate()[0].strip()
 
 def poke(port):
 	s = socket.socket()
@@ -44,7 +47,9 @@ def read_runbook(runbooks):
 		 	test_group_name = script['testGroup']
 		 	common_client_properties = script.get('commonClientProperties', '')
 		 	common_server_properties = script.get('commonServerProperties', '')
-		 	test_branch = script.get('branch')
+		 	current_branch = get_current_branch()
+		 	client_branch = script.get('clientBranch', current_branch)
+		 	server_branch = script.get('serverBranch', current_branch)
 		 	tests = script['tests']
 		 	test_list = []
 		 	for test in tests:
@@ -53,62 +58,94 @@ def read_runbook(runbooks):
 		 		server_properties = test.get('serverProperties', '')
 		 		test_instance = Test(name, ' '.join((client_properties, common_client_properties)), ' '.join((server_properties, common_server_properties)))
 		 		test_list.append(test_instance)
-		 	test_group = TestGroup(test_group_name, test_branch, test_list)
+		 	test_group = TestGroup(test_group_name, client_branch, server_branch, test_list)
 		 	test_groups.append(test_group)
 
 	return test_groups
 
-def run(directory, test_group, gradle, cwd, verbose):
+def run(directory, test_group, gradle, cwd, verbose, build_dir):
 	file_path = os.path.join(directory, test_group.name)
-	with open(file_path, "w") as out:
-		def write_to_file(msg):
-			print(msg, file=out)
+	gc_out_dir = os.path.join(directory, "{0}-gc".format(test_group.name))
+	result_out_dir = os.path.join(directory, "{0}-result".format(test_group.name))
+	#if not os.path.exists(gc_out_dir):
+	#	os.makedirs(gc_out_dir)
+	if not os.path.exists(result_out_dir):
+		os.makedirs(result_out_dir)
 
-		for test in test_group.tests:
-			for stage in ("1", "2"):
-				test_name = "{0}-{1}".format(test.name, stage)
-				logger.info("processing test: {0}".format(test_name))
-				write_to_file(test_name)
-				write_to_file("client properties: {0}".format(test.client_properties))
-				write_to_file("server properties: {0}".format(test.server_properties))
-				logger.info("starting server...")
-				server_process = run_gradle(gradle, 'runHttpServer', test.server_properties, cwd)
+	current_branch = get_current_branch()
+	current_wd = os.getcwd()
 
-				i = 0
-				while not poke(8082):
-					i = i + 1
-					assert i < 300, "Server didn't start within 5 minutes."
-					sleep(1)
+	hack_flag = False
 
-				logger.info("started server...")
-				logger.info("starting client and running test...")
-				client_process = run_gradle(gradle, 'runHttpRestClient', test.client_properties, cwd)
-				test_done = False
-				test_has_error = False
-				for raw_line in client_process.stdout:
-					line = raw_line.rstrip(os.linesep)
-					if verbose:
-						logger.debug(line)
-					if error_or_warn.search(line):
-						logger.warn("client error or warn: " + line)
-						test_has_error = True
-					elif done.search(line):
-						test_done = True
+	if test_group.client_branch == test_group.server_branch and test_group.client_branch == current_branch:
+		pass
+	elif test_group.client_branch == test_group.server_branch:
+		check_call(['git', 'checkout', test_group.client_branch])
+	else:
+		hack_flag = True
+		check_call(['git', 'checkout', test_group.server_branch], cwd=copy_dir)
+		if test_group.client_branch != current_branch:
+			check_call(['git', 'checkout', test_group.client_branch])
 
-					if test_done and not empty.match(line):
-						write_to_file(line)
+	for test in test_group.tests:
+		for stage in ("1", "2"):
+			test_name = "{0}-{1}".format(test.name, stage)
+			logger.info("processing test: {0}".format(test_name))
+			logger.info(test_name)
+			logger.info("client properties: {0}".format(test.client_properties))
+			logger.info("server properties: {0}".format(test.server_properties))
+			logger.info("starting server...")
 
-				if test_has_error:
-					write_to_file("This test has error or warning")
+			if hack_flag:
+				os.chdir(copy_dir)
+				print("running server at dir {0}".format(os.getcwd()))
 
+			server_process = run_gradle(gradle, 'runHttpServer', test.server_properties, cwd)
+
+			i = 0
+			while not poke(8082):
+				i = i + 1
+				assert i < 300, "Server didn't start within 5 minutes."
+				sleep(1)
+
+			logger.info("started server...")
+
+			if hack_flag:
+				os.chdir(current_wd)
+				print("set wd back to {0}".format(os.getcwd()))
+
+
+			logger.info("starting client and running test...")
+			client_process = run_gradle(gradle, 'runHttpRestClient', test.client_properties, cwd)
+			if verbose:
+				while client_process.poll() is None:
+					output = client_process.stdout.readline()
+					if output:
+						logger.info(output.rstrip())
+			else:
 				client_process.wait()
-				logger.info("stopped client...")
-				server_process.kill()
-				server_process.wait()
-				logger.info("stopped server...")
-				write_to_file("============================")
 
-		logger.info("finished processing of test group: {0}".format(test_group.name))
+
+			logger.info("stopped client...")
+			server_process.kill()
+			server_process.wait()
+			logger.info("stopped server...")
+
+			call('jps | grep "RunHttpServer\|RunHttpRestClient\|GradleDaemon\|GradleMain" | cut -d " " -f 1 | xargs kill -9', shell=True)
+			sleep(10)
+			assert not poke(8082), "what, server still up?"
+
+			logger.info("copying results...")
+			for result_file in glob.glob(os.path.join(build_dir, 'r2-perf-test/*.output')):
+				shutil.move(result_file, os.path.join(result_out_dir, "{0}-{1}".format(test_name, os.path.basename(result_file))))
+			#logger.info("copying gc logs...")
+			#for gc_file in glob.glob(os.path.join(build_dir, 'r2-perf-test/logs/gc/*.log')):
+				#shutil.copy(gc_file, os.path.join(gc_out_dir, "{0}-{1}".format(test_name, os.path.basename(gc_file))))
+
+	os.chdir(current_wd)
+	check_call(['git', 'checkout', current_branch])
+	logger.info("finished processing of test group: {0}".format(test_group.name))
+
 
 
 def run_gradle(gradle, gradle_cmd, properties, cwd):
@@ -123,6 +160,7 @@ if __name__ == '__main__':
 	parser.add_argument('--out', type=str, default='./out', help='output dir')
 	parser.add_argument('--gradle', type=str, default='ligradle', help='gradle to run')
 	parser.add_argument('--cwd', type=str, default='.', help='child process work directory')
+	parser.add_argument('--build-dir', dest='build_dir', type=str, default='../build', help='build directory')
 	parser.add_argument('--verbose', dest='verbose', action='store_true')
 
 	args = parser.parse_args()
@@ -138,19 +176,12 @@ if __name__ == '__main__':
 
 	test_groups = read_runbook(args.runbooks)
 	start = time()
+	current_branch = get_current_branch()
 	os.setpgrp() # create new process group, become its leader
 	try:
 		for test_group in test_groups:
 			logger.info("processing test group: {0}".format(test_group.name))
-			if test_group.branch:
-				check_call(['git', 'checkout', test_group.branch])
-				logger.info("checked out branch {0}".format(test_group.branch))
-			try:
-				run(directory, test_group, args.gradle, args.cwd, args.verbose)
-			finally:
-				if test_group.branch:
-					check_call(['git', 'checkout', '@{-1}'])
-					logger.info("resumed git repo to previous branch")
+			run(directory, test_group, args.gradle, args.cwd, args.verbose, args.build_dir)
 		stop = time()
 		print("Took {0} seconds to finish tests".format(stop-start))
 	except:
@@ -158,6 +189,7 @@ if __name__ == '__main__':
 		logger.exception(e)
 		raise e
 	finally:
+		check_call(['git', 'checkout', current_branch])
 		os.killpg(0, SIGKILL) # kill all processes in this group
 
 
