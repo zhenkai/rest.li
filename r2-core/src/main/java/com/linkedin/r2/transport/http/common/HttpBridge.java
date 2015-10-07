24d04632cc4ba9f67a4378365b29fcad0873f0c7
Zhenkai Zhu <zzhu@linkedin.com>
2015-10-07 12:53:01 -0700

    add back rest methods to HttpBridge

--- r2-core/src/main/java/com/linkedin/r2/transport/http/common/HttpBridge.java
+++ r2-core/src/main/java/com/linkedin/r2/transport/http/common/HttpBridge.java
@@ -19,6 +19,10 @@ package com.linkedin.r2.transport.http.common;
 
 
 import com.linkedin.r2.RemoteInvocationException;
+import com.linkedin.r2.message.rest.RestException;
+import com.linkedin.r2.message.rest.RestRequest;
+import com.linkedin.r2.message.rest.RestRequestBuilder;
+import com.linkedin.r2.message.rest.RestResponse;
 import com.linkedin.r2.message.rest.RestStatus;
 import com.linkedin.r2.message.stream.StreamException;
 import com.linkedin.r2.message.stream.StreamRequest;
@@ -36,6 +40,94 @@ import java.util.Map;
  */
 public class HttpBridge
 {
+  /**
+   * Wrap application callback for incoming RestResponse with a "generic" HTTP callback.
+   *
+   * @param callback the callback to receive the incoming RestResponse
+   * @param request the request, used only to provide useful context in case an error
+   *          occurs
+   * @return the callback to receive the incoming HTTP response
+   */
+  public static TransportCallback<RestResponse> restToHttpCallback(final TransportCallback<RestResponse> callback,
+                                                                   RestRequest request)
+  {
+    final URI uri = request.getURI();
+    return new TransportCallback<RestResponse>()
+    {
+      @Override
+      public void onResponse(TransportResponse<RestResponse> response)
+      {
+        if (response.hasError())
+        {
+          response =
+              TransportResponseImpl.error(new RemoteInvocationException("Failed to get response from server for URI "
+                      + uri,
+                      response.getError()),
+                  response.getWireAttributes());
+        }
+        else if (!RestStatus.isOK(response.getResponse().getStatus()))
+        {
+          response =
+              TransportResponseImpl.error(new RestException(response.getResponse(),
+                      "Received error "
+                          + response.getResponse()
+                          .getStatus()
+                          + " from server for URI "
+                          + uri),
+                  response.getWireAttributes());
+        }
+
+        callback.onResponse(response);
+      }
+    };
+  }
+
+  /**
+   * Combine the specified {@link RestRequest} and map of headers to construct a new
+   * {@link RestRequest}.
+   *
+   * @param request the {@link RestRequest} to be used as a source object.
+   * @param headers the headers to set on the request. These override any headers on the
+   *          source request.
+   * @return a new {@link RestRequest} which combines the specified request and headers.
+   */
+  public static RestRequest toRestRequest(RestRequest request, Map<String, String> headers)
+  {
+    return new RestRequestBuilder(request)
+        .unsafeSetHeaders(headers)
+        .build();
+  }
+
+  /**
+   * Wrap transport callback for outgoing "generic" http response with a callback to pass
+   * to the application REST server.
+   *
+   * @param callback the callback to receive the outgoing HTTP response
+   * @return the callback to receive the outgoing REST response
+   */
+  public static TransportCallback<RestResponse> httpToRestCallback(final TransportCallback<RestResponse> callback)
+  {
+    return new TransportCallback<RestResponse>()
+    {
+      @Override
+      public void onResponse(TransportResponse<RestResponse> response)
+      {
+        if (response.hasError())
+        {
+          final Throwable ex = response.getError();
+          if (ex instanceof RestException)
+          {
+            callback.onResponse(TransportResponseImpl.success(((RestException) ex).getResponse(),
+                response.getWireAttributes()));
+            return;
+          }
+        }
+
+        callback.onResponse(response);
+      }
+    };
+  }
+
   /**
    * Wrap application callback for incoming RestResponse with a "generic" HTTP callback.
    *
